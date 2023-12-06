5af295de2b7790b3158be739677d521666a71a94
Marco Leskovar <leskovarmarco@gmail.com>
2023-12-06 20:57:42 -0300

    user model

---
+++ src/dao/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const userCollection = 'users'
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: String
+    
+})
+
+const UserModel = mongoose.model(userCollection, userSchema)
+
+export default UserModel
