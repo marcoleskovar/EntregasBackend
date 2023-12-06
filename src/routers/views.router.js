695c695ca75ac1791a11ed12922adfa269f8b1d0
Marco Leskovar <leskovarmarco@gmail.com>
2023-12-06 17:37:50 -0300

    acorto el codigo de views.router

--- src/routers/views.router.js
+++ src/routers/views.router.js
@@ -3,26 +3,33 @@ import ProductsModel from '../dao/models/products.model.js'
 
 const router = Router()
 
-router.get('/', async (req, res) => {
-    try{
-        const limit = parseInt(req.query?.limit ?? 10)
-        const page = parseInt(req.query?.page ?? 1)
-        const sort = req.query?.sort?? ''
 
-        const sortOptions = sort === 'asc' ? { price: 1 } : (sort === 'des' ? { price: -1 } : {})
+const queryParams = async (req) => {
+    const limit = parseInt(req.query?.limit ?? 10)
+    const page = parseInt(req.query?.page ?? 1)
+    const sort = req.query?.sort ?? ''
+    const sortOptions = sort === 'asc' ? { price: 1 } : (sort === 'des' ? { price: -1 } : {})
 
-        const result = await ProductsModel.paginate({}, {
+    const result = await ProductsModel.paginate({}, {
             page,
             limit,
             sort: sortOptions,
             lean: true
-        })
-        result.sort = sort
-        res.render('home', {})
+    })
+    result.sort = sort
+
+    return result
+}
+
+
+router.get('/', async (req, res) => {
+    try{
+        const result = await queryParams(req)
         const prevPage = result.prevPage
         const nextPage = result.nextPage
         const hasPrevPage = result.hasPrevPage
         const hasNextPage = result.hasNextPage
+        res.render('home', {})
         return res.status(200).json({
             status: 'success', 
             payload: result.docs, 
@@ -37,25 +44,14 @@ router.get('/', async (req, res) => {
         })
     }
     catch(e){
+        console.log(e);
         return res.status(500).json({success: false, error: e.message, detail: e})
     }
 })
 
 router.get('/products', async (req, res) => {
     try{
-        const limit = parseInt(req.query?.limit ?? 10)
-        const page = parseInt(req.query?.page ?? 1)
-        const sort = req.query?.sort?? ''
-
-        const sortOptions = sort === 'asc' ? { price: 1 } : (sort === 'des' ? { price: -1 } : {})
-
-        const result = await ProductsModel.paginate({}, {
-            page,
-            limit,
-            sort: sortOptions,
-            lean: true
-        })
-        result.sort = sort
+        const result = await queryParams(req)
         res.render('products', result)
     }
     catch(e){
@@ -65,20 +61,7 @@ router.get('/products', async (req, res) => {
 
 router.get('/realTimeProducts', async (req, res) => {
     try{
-        const limit = parseInt(req.query?.limit ?? 10)
-        const page = parseInt(req.query?.page ?? 1)
-        const sort = req.query?.sort?? ''
-
-        const sortOptions = sort === 'asc' ? { price: 1 } : (sort === 'des' ? { price: -1 } : {})
-
-        const result = await ProductsModel.paginate({}, {
-            page,
-            limit,
-            sort: sortOptions,
-            lean: true
-        })
-        result.sort = sort
-        //const productList = await ProductsModel.find().lean().exec()
+        const result = await queryParams(req)
         res.render('realTimeProducts', result)
     }
     catch(e){
